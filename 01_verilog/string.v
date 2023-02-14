d1ca0453ef6b42f39a8b0f94271e7b1d75c3e538
MQ <mq@4mq.xyz>
2023-02-14 18:40:08 +0100

    update readme and string

--- 01_verilog/string.v
+++ 01_verilog/string.v
@@ -1,10 +1,21 @@
 module string;
-    reg [8*5:0] str = "abcde";
+
+    reg [8*5:1] str = "abcde";
+    reg [7:0] char;
+    integer i, j, base;
 
     initial begin
 
         $display ("%b", str);
         $display ("%s", str);
+
+        for (i = 0; i < 5; i += 1) begin
+            base = i*8 + 1;
+            for (j = 0; j < 8; j += 1) begin
+                char[j] = str[base + j];
+            end
+            $display ("From: %d To: %d Char: %s %d", base, base+7, char, char);
+        end
 
     end
 
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # From the transistor to web browser
 
 From understanding basic hardware stuff to web browser. Based on [geohot/fromthetransistor](https://github.com/geohot/fromthetransistor).
+
+**Materials:**
+- [nand2tetris](https://www.nand2tetris.org/)
+- **UART**
+    - [Understanding UART (YTB)](https://www.youtube.com/watch?v=sTHckUyxwp8)
